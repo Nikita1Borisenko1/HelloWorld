e7d3de5f8a826507925c770998c946ce908e56fc
nikit <nikit@DESKTOP-NMM9RMU>
2024-03-12 08:59:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; /// Прописка консоли чтобы каждый раз не писать
+namespace HellowWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args) /// точка входа обязательная
+        {
+            WriteLine("Привет мир!"); /// Вывод текста
+            ReadKey(); /// Задержка экрана
+        }
+    }
+}
